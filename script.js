84d18d0d8c79aceb926b198870d660b18da81757
Karen Roh <karenroh@hotmail.com>
2021-03-05 21:24:47 -0500

    made code cleaner by adding descriptive comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assign characters to each corresponding variable
+// Assign characters to each corresponding character type
 var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
 var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numbers = "1234567890";
@@ -26,19 +26,19 @@ function generatePassword() {
 
   //Ask user for password length 
   var passLength = prompt("How many characters would you like your password to contain?", "8");
-// If length of password is less than 8 or greater than 128 return an alert
-if (isNaN(passLength)) {
+  // If length of password is less than 8 or greater than 128 return an alert
+  if (isNaN(passLength)) {
   alert("Please enter a number.")
   return "";
-}  
-if (passLength < 8) {
+  }  
+  if (passLength < 8) {
     alert("Password length must be at least 8 characters.")
     return "";
-  }
+ }
   else if (passLength > 128) {
     alert("Password length must not be more than 128 characters.");
     return "";
-  }
+ }
   
   //Ask user if lowercase characters are desired
   var passLower = confirm("Click OK to confirm including lowercase characters.")
@@ -68,15 +68,13 @@ if (passLength < 8) {
     allowedCharacters += symbols;
   }
 
-//  if user says no for all characters, make error msg pop up
-if (passUpper === false && passLower === false && passNumber === false && passSymbol === false) {
+//  If user doesn't select any of the character types, make error msg pop up
+  if (passUpper === false && passLower === false && passNumber === false && passSymbol === false) {
   alert("Please select at least one character type.")
   return "";
-}
+  }
 
-
-
-  //length, allowedCharacters 'qwertyQWERTY123455'
+  // Randomize and combine all selected characters and return final password into #password input
   for (let i = 0; i < passLength; i++) {
     var randomIndex = Math.floor(Math.random() * allowedCharacters.length);
 
@@ -85,8 +83,6 @@ if (passUpper === false && passLower === false && passNumber === false && passSy
   }
   return finalPassword;
 }
-
-
 
 
 // Add event listener to generate button (given)
